82a1d33f3be2571c1a9efec03391877f75865ac4
Xiong JK <xiong@comet.la>
2026-03-07 17:36:57 +0700

    tt

--- my_ecommerce_app/ios/Flutter/flutter_export_environment.sh
+++ my_ecommerce_app/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
+<<<<<<< Updated upstream
 export "FLUTTER_ROOT=/usr/local/share/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/arnaud/Documents/GitHub/myecomerceapp/my_ecommerce_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
+=======
+export "FLUTTER_ROOT=C:\Users\USER\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\USER\OneDrive\Documents\GitHub\myecomerceapp\my_ecommerce_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+>>>>>>> Stashed changes
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
--- my_ecommerce_app/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ my_ecommerce_app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
+<<<<<<< Updated upstream
 export "FLUTTER_ROOT=/usr/local/share/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/arnaud/Documents/GitHub/myecomerceapp/my_ecommerce_app"
+=======
+export "FLUTTER_ROOT=C:\Users\USER\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\USER\OneDrive\Documents\GitHub\myecomerceapp\my_ecommerce_app"
+>>>>>>> Stashed changes
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
